45fb1500c5e0fc114c554bda5efec45db73e63e1
unknown <vnqq23021998@gmail.com>
2026-04-22 21:50:47 +0700

    update cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'http://103.82.135.143:5173'
+  ],
+  credentials: true
+}));
 
 app.use(express.json());
 
