f31862e16800d53d94380ef2f8737dadf99599bf
unknown <vnqq23021998@gmail.com>
2026-04-22 21:26:22 +0700

    ok

--- src/models/AccountChecked.js
+++ src/models/AccountChecked.js
@@ -1,21 +1,14 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema(
-  {
-    accountId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Account',
-      required: true,
-      unique: true
-    },
-    username: String,
-    password: String,
-    phone: String,
-    balance: Number,
-    status: String,
-    message: String
-  },
-  { timestamps: true }
-);
+const schema = new mongoose.Schema({
+  accountId: { type: String, unique: true },
+  username: String,
+  password: String,
+  displayName: String,
+  phone: String,
+  balance: Number,
+  status: String,
+  message: String
+}, { timestamps: true });
 
 module.exports = mongoose.model('AccountChecked', schema);
