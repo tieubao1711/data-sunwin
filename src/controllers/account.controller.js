4c9daeee7ac4180ba8ff8858dcdfc589f9f0a00c
unknown <vnqq23021998@gmail.com>
2026-04-22 19:48:51 +0700

    check unique account

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,8 +1,17 @@
 const Account = require('../models/Account');
 
 exports.create = async (req, res) => {
-  const data = await Account.create(req.body);
-  res.json(data);
+  try {
+    const data = await Account.create(req.body);
+    res.json(data);
+  } catch (err) {
+    if (err.code === 11000) {
+      return res.status(409).json({
+        message: 'Duplicate account (username + password)'
+      });
+    }
+    res.status(500).json({ message: err.message });
+  }
 };
 
 exports.getAll = async (req, res) => {
--- src/models/Account.js
+++ src/models/Account.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-  fileName: String,
-  username: String,
-  password: String
-}, { timestamps: true });
+const schema = new mongoose.Schema(
+  {
+    fileName: String,
+    username: String,
+    password: String
+  },
+  { timestamps: true }
+);
+
+// unique theo combo username + password
+schema.index({ username: 1, password: 1 }, { unique: true });
 
 module.exports = mongoose.model('Account', schema);
