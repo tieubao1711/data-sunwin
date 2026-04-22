f40fe32a756937576ad3f8f401c6e914b5230c1c
unknown <vnqq23021998@gmail.com>
2026-04-22 21:21:27 +0700

    update account checked

--- src/controllers/accountChecked.controller.js
+++ src/controllers/accountChecked.controller.js
@@ -1,11 +1,40 @@
 const AccountChecked = require('../models/AccountChecked');
 
 exports.create = async (req, res) => {
-  const data = await AccountChecked.create(req.body);
-  res.json(data);
+  try {
+    const { accountId } = req.body;
+
+    if (!accountId) {
+      return res.status(400).json({
+        message: 'accountId is required'
+      });
+    }
+
+    const data = await AccountChecked.findOneAndUpdate(
+      { accountId },
+      { $set: req.body },
+      {
+        new: true,
+        upsert: true,
+        runValidators: true
+      }
+    );
+
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
 };
 
 exports.getAll = async (req, res) => {
-  const data = await AccountChecked.find().populate('accountId');
-  res.json(data);
+  try {
+    const data = await AccountChecked.find().populate('accountId');
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
 };
--- src/models/AccountChecked.js
+++ src/models/AccountChecked.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-  accountId: { type: mongoose.Schema.Types.ObjectId, ref: 'Account' },
-  username: String,
-  password: String,
-  phone: String,
-  balance: Number,
-  status: String,
-  message: String
-}, { timestamps: true });
+const schema = new mongoose.Schema(
+  {
+    accountId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Account',
+      required: true,
+      unique: true
+    },
+    username: String,
+    password: String,
+    phone: String,
+    balance: Number,
+    status: String,
+    message: String
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('AccountChecked', schema);
